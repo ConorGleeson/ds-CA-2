0b0d6dfc23f34f5315f23062f9e925ab8df8dde2
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2024-01-02 13:54:47 +0000

    added rejection email to mailer lambda but not getting emails for some reason

--- lambdas/mailer.ts
+++ lambdas/mailer.ts
@@ -19,8 +19,7 @@ type ContactDetails = {
   message: string;
 };
 
-const client = new SESClient({ region: "eu-west-1" });
-
+const client = new SESClient({ region: SES_REGION });
 export const handler: SQSHandler = async (event: any) => {
   console.log("Event ", event);
   for (const record of event.Records) {
@@ -32,19 +31,26 @@ export const handler: SQSHandler = async (event: any) => {
       for (const messageRecord of snsMessage.Records) {
         const s3e = messageRecord.s3;
         const srcBucket = s3e.bucket.name;
-        // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+        const srcKey = decodeURIComponent(s3e.object.key.replace(/+/g, " "));
         try {
+          // Check if the uploaded file is an image
+          if (!imageCheck(srcKey)) {
+            throw new Error("Uploaded file is not an image");
+          }
+
           const { name, email, message }: ContactDetails = {
             name: "The Photo Album",
             email: SES_EMAIL_FROM,
-            message: `We received your Image. Its URL is s3://${srcBucket}/${srcKey}`,
+            message:" We received your Image. Its URL is s3://${srcBucket}/${srcKey}",
           };
           const params = sendEmailParams({ name, email, message });
           await client.send(new SendEmailCommand(params));
         } catch (error: unknown) {
           console.log("ERROR is: ", error);
-          // return;
+          if (error instanceof Error) {
+            const errorMessage = error.message || "Unknown error occurred";
+            await sendRejectionEmail(SES_EMAIL_TO, SES_EMAIL_FROM, errorMessage);
+          }
         }
       }
     }
@@ -77,6 +83,9 @@ function sendEmailParams({ name, email, message }: ContactDetails) {
   return parameters;
 }
 
+
+
+
 function getHtmlContent({ name, email, message }: ContactDetails) {
   return `
     <html>
@@ -100,4 +109,35 @@ function getTextContent({ name, email, message }: ContactDetails) {
         ✉️ ${email}
     ${message}
   `;
+}
+
+//method to check filetypes
+function imageCheck (file: string) {
+  const fileTypes = [".jpeg, .png"]
+  const type  = file.toLowerCase().slice(file.lastIndexOf('.'))
+  return fileTypes.includes(type)
+
+}
+
+async function sendRejectionEmail(toEmail: string, fromEmail: string, errorMessage: string) {
+  const params = {
+    Destination: {
+      ToAddresses: [toEmail],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `<p>There was an error processing your image. The error is: ${errorMessage}</p>`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: `Error processing your image`,
+      },
+    },
+    Source: fromEmail,
+  };
+
+   await client.send(new SendEmailCommand(params));
 }
